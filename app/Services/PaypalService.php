6dcfd14e973fe77c8d1b375d9a5dfce7455c3d07
Luis Alberto Amador Zamorano <luis.alberto@webdecero.com>
2021-06-11 20:08:43 -0500

    inicio de integracion metodo de pago con paypal

---
+++ app/Services/PaypalService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExternalServices;
+
+class PaypalService
+{
+
+    use ConsumesExternalServices;
+
+    protected $baseUrl;
+
+    protected $clientId;
+
+    protected $clientSecret;
+
+    public function __construct()
+    {
+        $this->baseUrl = config('services.paypal.base_uri');
+        $this->clientId = config('services.paypal.client_id');
+        $this->clientSecret = config('services.paypal.client_secret');
+    }
+
+
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
+    {
+        $headers['Authorization'] = $this->resolveAccessToken();
+    }
+
+
+    public function decodeResponse($response)
+    {
+        return json_decode($response);
+    }
+
+    public function resolveAccessToken()
+    {
+        $credentials = base64_encode("{$this->clientId}:{$this->clientSecret}");
+
+        return "Basic {$credentials}";
+    }
+
+}
